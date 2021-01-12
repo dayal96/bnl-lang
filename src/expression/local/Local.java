0765e26fb91fe767f7fdc258a3692f47b1eac0e0
Amogh Dayal <amoghdayal@gmail.com>
2021-01-11 19:05:04 -0500

    Local scopes implemented

--- src/expression/local/Local.java
+++ src/expression/local/Local.java
@@ -2,6 +2,7 @@ package expression.local;
 
 import environment.IEnvironment;
 import environment.LocalContext;
+import environment.SymbolTable;
 import expression.IExpression;
 import expression.type.Type;
 
@@ -9,27 +10,39 @@ import java.util.List;
 
 public class Local implements IExpression {
 
-    private final IEnvironment localContext;
+    private final List<LocalDefinition> localDefinitions;
     private final IExpression body;
 
     /**
      * Create a Local expression with given context that evaluates given expression.
-     * @param localContext  The context bound within this local expression.
+     * @param localDefinitions  The context bound within this local expression.
      * @param body          The expression that would be evaluated within the local context.
      */
-    public Local(IEnvironment localContext, IExpression body) {
-        this.localContext = localContext;
+    public Local(List<LocalDefinition> localDefinitions, IExpression body) {
+        this.localDefinitions = localDefinitions;
         this.body = body;
     }
 
     @Override
     public IExpression evaluate(IEnvironment environment) throws Exception {
-        return this.body.evaluate(new LocalContext(environment, this.localContext));
+        return this.body.evaluate(this.addLocalDefinitions(environment));
     }
 
     @Override
-    public IExpression evaluate(List<IExpression> operands, IEnvironment environment) throws Exception {
-        return this.body.evaluate(operands, new LocalContext(environment, this.localContext));
+    public IExpression evaluate(List<IExpression> operands, IEnvironment environment)
+        throws Exception {
+        return this.body.evaluate(operands, this.addLocalDefinitions(environment));
+    }
+
+    private IEnvironment addLocalDefinitions(IEnvironment environment) throws Exception {
+        SymbolTable localContext = new SymbolTable();
+        IEnvironment fullContext = new LocalContext(environment, localContext);
+
+        for(LocalDefinition def : this.localDefinitions) {
+            localContext.addEntry(def.name, def.value.evaluate(localContext));
+        }
+
+        return fullContext;
     }
 
     @Override
@@ -39,6 +52,6 @@ public class Local implements IExpression {
 
     @Override
     public String toString() {
-        return "(local [" + this.localContext.toString() + "]; " + this.body.toString() + ")";
+        return "(local " + this.localDefinitions.toString() + "; " + this.body.toString() + ")";
     }
 }
